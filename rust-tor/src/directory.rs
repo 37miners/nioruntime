fb3a3a867177b29bd9733bba70939f65c343cec4
Rubius Hagrid <rubius_hagrid37@protonmail.com>
2022-02-22 17:07:40 -0800

    add test for directory loading

--- rust-tor/src/directory.rs
+++ rust-tor/src/directory.rs
@@ -12,18 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use nioruntime_err::Error;
+use nioruntime_err::{Error, ErrorKind};
 use nioruntime_log::*;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
 
 debug!();
 
+/// Tor Host. Holds information about a tor host.
 #[derive(Debug, Clone)]
 pub struct TorHost {
-	addr: String,
+	pub addr: String,
 }
 
+/// Tor directory. Holds information about the tor directory.
 pub struct TorDirectory {
 	directory_servers: Vec<String>,
 	guards: Vec<TorHost>,
@@ -39,9 +41,18 @@ impl TorDirectory {
 		}
 	}
 
+	pub fn guards(&self) -> &Vec<TorHost> {
+		&self.guards
+	}
+
+	pub fn relays(&self) -> &Vec<TorHost> {
+		&self.relays
+	}
+
+	// load the guard/relay nodes from the tor directory servers. Try in order specified in [`TorDirectory::new`].
 	pub fn load(&mut self) -> Result<(), Error> {
 		let mut i = 0;
-		let (guards, relays) = loop {
+		(self.guards, self.relays) = loop {
 			match self.load_from_server(&self.directory_servers[i]) {
 				Ok(info) => break info,
 				Err(e) => {
@@ -54,25 +65,33 @@ impl TorDirectory {
 			}
 
 			i += 1;
+
+			if i >= self.directory_servers.len() {
+				return Err(ErrorKind::ApplicationError(
+					"Not able to contact any directory servers".to_string(),
+				)
+				.into());
+			}
 		};
 
-		info!("Returned guards:");
+		debug!("Returned guards:");
 		let mut count = 0;
-		for guard in guards {
-			info!("guard[{}]={:?}", count, guard);
+		for guard in &self.guards {
+			debug!("guard[{}]={:?}", count, guard);
 			count += 1;
 		}
 
-		info!("Returned relays:");
+		debug!("Returned relays:");
 		let mut count = 0;
-		for relay in relays {
-			info!("relay[{}]={:?}", count, relay);
+		for relay in &self.relays {
+			debug!("relay[{}]={:?}", count, relay);
 			count += 1;
 		}
 
 		Ok(())
 	}
 
+	// Load from the server
 	fn load_from_server(
 		&self,
 		directory_server: &String,
@@ -94,7 +113,6 @@ impl TorDirectory {
 				let split: Vec<&str> = line.split(" ").collect();
 				let addr = format!("{}:{}", split[6], split[7]);
 				next_host = Some(TorHost { addr });
-				//relays.push(TorHost{ addr });
 			}
 			if &line[..2] == "s " {
 				let last_host = next_host.clone().unwrap();
@@ -112,18 +130,39 @@ impl TorDirectory {
 mod test {
 	use crate::directory::*;
 	use nioruntime_err::Error;
+	use std::net::TcpListener;
 
 	#[test]
 	fn test_directory_load() -> Result<(), Error> {
-		let directories = vec![
-			"45.66.33.45:80".to_string(),
-			"193.23.244.244:80".to_string(),
-			"66.111.2.131:80".to_string(),
-		];
+		let listener = TcpListener::bind("127.0.0.1:8093")?;
+
+		// make a mock directory server that returns two entries.
+		std::thread::spawn(move || {
+			for stream in listener.incoming() {
+				let mut stream = stream.unwrap();
+				let buf = &mut [0u8; 100];
+				let len = stream.read(&mut buf[..]).unwrap();
+				stream.write(
+b"r plithismos GRdTUVgUe1VLSfpLIkV1HB7yHS4 uf5xsxMQy/gJxTNnCtlBtnXOzCg 2022-02-22 14:02:49 45.61.184.239 9001 9030\r\n\
+s Fast Guard Running Stable V2Dir Valid\r\n\
+r emandeman44678gudno GTlGnR6Jj3Z0pV8Jvkbul0LDP6Q QUKIW/cbXHxGNhzA4BN2b0Shmss 2022-02-22 13:00:31 82.220.109.130 9001 0
+s Fast HSDir Running Stable V2Dir Valid").unwrap();
+			}
+		});
+
+		let directories = vec!["127.0.0.1:8093".to_string()];
 
 		let mut tor_dir = TorDirectory::new(directories);
 
 		tor_dir.load()?;
+
+		let relays = tor_dir.relays();
+		assert_eq!(relays.len(), 1);
+		let guards = tor_dir.guards();
+		assert_eq!(guards.len(), 1);
+
+		assert_eq!(relays[0].addr, "82.220.109.130:9001");
+		assert_eq!(guards[0].addr, "45.61.184.239:9001");
 
 		Ok(())
 	}
